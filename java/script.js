b455ca66c385230d46d929f2ff7dbcfcf60c7a9d
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-01-16 12:38:49 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -3,3 +3,14 @@ let searchForm = document.querySelector('.header .search-form')
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active')
 }
+
+let navbar = document.querySelector('.header .navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active')
+    navbar.classList.remove('active')
+}
